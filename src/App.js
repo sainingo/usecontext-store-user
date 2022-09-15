8a579bb447ae9af4d37933332a7770c9fbca4389
sainingo <nickmentor421@gmail.com>
2022-09-15 12:32:35 +0300

    Add react router & setup app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import About from "./components/About";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
